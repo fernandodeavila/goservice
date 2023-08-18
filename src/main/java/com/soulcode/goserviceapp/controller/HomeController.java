37a9603cf7d07d135aa84e1c32e2bd004a7f99b1
fernandodeavila <fernandeavila55@gmail.com>
2023-08-18 11:10:30 -0300

    teste

--- src/main/java/com/soulcode/goserviceapp/controller/HomeController.java
+++ src/main/java/com/soulcode/goserviceapp/controller/HomeController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Controller //conversa direto com  o front
+@Controller //conversa direto com  o frontghg
 @RequestMapping(value = "/")
 public class HomeController {
     @GetMapping
